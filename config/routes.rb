487e395e3a425e31b3b86ab102357d56d67eedd7
toshi kuramochi <toshi083073@gmail.com>
2020-01-21 23:49:06 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'example/test1'
-  get 'example/test2'
-  get 'example/test3'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
