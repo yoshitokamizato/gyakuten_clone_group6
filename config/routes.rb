2b8d49745eb5bf03b96874ce7d1ae3c7e7e0d675
koumura2501 <koumura2501@gmail.com>
2020-01-23 22:58:46 +0900

    10_task_kou

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  devise_for :users
   root to: 'movies#index'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users
+  ActiveAdmin.routes(self)
+
 end
