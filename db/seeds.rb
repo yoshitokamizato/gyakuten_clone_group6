4ab007020f4ae9ad9def67b1a181074a58b7ac9f
koumura2501 <koumura2501@gmail.com>
2020-02-28 00:48:33 +0900

    delete unnecessary

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@
 AdminUser.create!(:email => 'admin@example.com', :password => 'password')
 #一般ユーザー
 User.create!(email: "test@example.com", password: "password")
-Question.create!(title: "質問１", content: "内容１", question_id: 1)
-Question.create!(title: "質問２", content: "内容２", question_id: 2)
-Question.create!(title: "質問３", content: "内容３", question_id: 3)
-Question.create!(title: "質問４", content: "内容４", question_id: 4)
-Question.create!(title: "質問５", content: "内容５", question_id: 5)
+Question.create!(title: "質問１", content: "内容１")
+Question.create!(title: "質問２", content: "内容２")
+Question.create!(title: "質問３", content: "内容３")
+Question.create!(title: "質問４", content: "内容４")
+Question.create!(title: "質問５", content: "内容５")
 
 
 #movieのseed
